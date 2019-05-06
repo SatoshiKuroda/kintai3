f0f7def467c77ed7e05b073a43dba47a0232677c
Satoshi Kuroda <sk@SatoshinoiMac.local>
2019-05-07 01:10:50 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  def authenticate_admin_user!
+    authenticate_user!
+    unless current_user.admin?
+      flash[:alert] = "This area is restricted to administrators only."
+      redirect_to root_path
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      #User(一般ユーザ)がログインした時の処理
+    when AdminUser
+      #AdminUser（管理ユーザ）がログインしたときの処理
+      stored_location_for(resource) ||
+      if resource.is_a?(AdminUser)
+        root_path
+      else
+        super
+      end
+    end
+  end
+end
