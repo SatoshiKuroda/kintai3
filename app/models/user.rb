17bce767d4e50b71ed62c4fd6713121fcb348a11
Satoshi Kuroda <sk@SatoshinoiMac.local>
2019-05-07 01:49:51 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise  :database_authenticatable, :registerable,
+  devise  :database_authenticatable, 
           :recoverable, :rememberable, :validatable
-  enum role: { admin: 1,manager: 2,hoge_staff: 3 }
+  enum role: { admin: 1, manager: 2, hoge_staff: 3 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
